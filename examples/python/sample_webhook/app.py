1e8b4487a1b337dfa8c3b7266261d7c17862337e
Oren Ariel <oren@saivahc.com>
2023-04-21 15:08:14 -0700

    Python webhook example

---
+++ examples/python/sample_webhook/app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request
+import hashlib, hmac, json
+
+app = Flask(__name__)
+secret = '<YOUR WEBHOOK SECRET KEY>'
+
+# Signature verification
+def verifyHmac(receivedHmacHeader, receivedBody, secret):
+    hmacParts = receivedHmacHeader.split(' ')
+    receivedHmac = hmacParts[1]
+    hash = hmac.new(bytes(secret, 'UTF-8'), receivedBody, hashlib.sha256).hexdigest()
+    return receivedHmac == hash
+
+# Main webhook entry point
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Some initial basic verification
+    if 'signature' not in request.headers:
+        print("Signature doesn't exist")
+        return '', 500
+    else:
+        print('Signature:', request.headers['signature'])
+
+    # Verify source from signature
+    if not verifyHmac(request.headers['signature'], request.data, secret):
+        print('Signature verification failed')
+        return '', 500
+
+    print('Signature verified')
+
+    # Extract payload
+    payload = request.get_json()
+    print(json.dumps(payload, indent=4))
+
+    # Parse the actual payload here
+    # TODO
+
+    return 'Ok'
+
+if __name__ == '__main__':
+    print('Listening for log events on 7071')
+    app.run(port=7071)
